97e8e6ec0272645776f0f0a62568d3ebec57919f
Md.Shahriar Mridha Rahul <54900950+Rahul-1504022@users.noreply.github.com>
2022-07-05 22:25:53 +0600

    image added in header

---
+++ src/body/Body.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Body = () => {
+    return (
+        <div className="container">
+            <p>Body</p>
+        </div>
+    )
+}
+export default Body;
---
+++ src/header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="container header">
+            <img src="assets/img/my_img.jpg" width="20%" height="20%" />
+        </div>
+    )
+}
+export default Header;
