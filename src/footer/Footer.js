f9f6237c6b49cc11b60a72a33e621bd3c59feea4
Md.Shahriar Mridha Rahul <54900950+Rahul-1504022@users.noreply.github.com>
2022-07-06 00:17:40 +0600

    some css modified

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -2,11 +2,14 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <div>
-            <img src="assets/img/facebook.png" weight="30px" height="30px" />
-            <img src="assets/img/github.png" weight="30px" height="30px" />
-            <img src="assets/img/instagram.jpeg" weight="30px" height="30px" />
-            <img src="assets/img/twitter.png" weight="30px" height="30px" />
+        <div className="container footer">
+
+            <a href="#"><img src="assets/img/facebook.png" weight="30px" height="30px" /></a>
+            <a href="#"><img src="assets/img/github.png" weight="30px" height="30px" /></a>
+            <a href="#"><img src="assets/img/instagram.jpeg" weight="30px" height="30px" /></a>
+            <a href="#"><img src="assets/img/twitter.png" weight="30px" height="30px" /></a>
+            <br />
+            <p>&COPY;All Rights Reserved</p>
         </div>
     )
 }
